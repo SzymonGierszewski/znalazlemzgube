f2f36f2982e953353681ade959bfc0408bba6ae1
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-07-07 13:24:01 +0200

    add @DateTimeFormat to LocalDate in class Marker

--- src/main/java/pl/szymongierszewski/znalazlemzgube/model/Marker.java
+++ src/main/java/pl/szymongierszewski/znalazlemzgube/model/Marker.java
@@ -1,5 +1,7 @@
 package pl.szymongierszewski.znalazlemzgube.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
@@ -13,5 +15,6 @@ public class Marker {
     @NotNull
     private Finder finder;
     @PastOrPresent
-    private LocalDate localDate;
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    private LocalDate date;
 }
